17e2393869b16f1225c2ad511e41ba0c89604c80
LubovSurina <lvsbetula@mail.ru>
2024-07-25 18:08:46 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_homework=12
+number_of_hours_spent=1.5
+course_name='Python'
+time_one_task=number_of_hours_spent/count_homework
+print('Курс: ',course_name,', всего задач: ',count_homework, ', затрачено часов: ',number_of_hours_spent, ', среднее время выполнения ', time_one_task, 'часа')
